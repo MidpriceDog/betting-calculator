354449e4b3a41e8ff510a187c916696ed15d9bf4
MidpriceDog <ianpaul1313+1@gmail.com>
2023-03-10 00:57:55 -0500

    updated doc string

--- vig.py
+++ vig.py
@@ -18,16 +18,16 @@ def vig(odds_arr):
     """
     imp_prob_list = []
     for odds in odds_arr:
-        if odds > 0:
-            # uses formula 1-p/p = odds/100
-            implied_prob = 100/(100+odds)
-        else:
-            implied_prob = abs(odds)/((abs(odds) + 100))
+        implied_prob = implied_probability(odds)
         imp_prob_list.append(implied_prob)
     return sum(imp_prob_list) - 1
 
 
-if __name__ == "__main__":
-    odds_arr1 = [310, 2000, 110, 210]
-    odds_arr2 = [450, 3800, 125, 140]
-    print(vig(odds_arr2))
+def implied_probability(odds):
+    if odds > 0:
+        # uses formula 1-p/p = odds/100
+        implied_prob = 100/(100+odds)
+    else:
+        implied_prob = abs(odds)/((abs(odds) + 100))
+    return implied_prob
+
